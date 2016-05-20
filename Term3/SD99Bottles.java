d1c0d3ff8af64f209492f3269420b5879f08f260
Siddhant Devaru <siddhantdevaru@hotmail.com>
2016-05-20 12:53:27 -0600

    Create SD99Bottles.java

---
+++ Term3/SD99Bottles.java
@@ -0,0 +1,7 @@
+int b = 99;
+    
+    while (b >0) {
+      System.out.println(b + " bottles of soda on the wall " + b + " bottles of soda on the wall. Take one down, pass it around, " 
+                           + (b-1) + " bottles of soda on the wall");
+      b = b - 1;
+    }
